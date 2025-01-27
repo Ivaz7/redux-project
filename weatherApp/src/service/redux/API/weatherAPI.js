be167579832c57a978fde5644a9d0e599dbfa235
Ivaz <ivazreza@gmail.com>
2025-01-27 17:07:59 +0700

    try again

--- weatherApp/src/service/redux/API/weatherAPI.js
+++ weatherApp/src/service/redux/API/weatherAPI.js
@@ -10,8 +10,8 @@ export const weatherApi = createApi({
   endpoints: (builder) => ({
     getWeather: builder.query({
       query: ({ lat, lon, q }) => {
-        console.log("API Token:", apiToken); 
-
+        console.log("API Token:", import.meta.env.VITE_API_TOKEN); 
+        
         if (lat && lon) {
           return {
             url: 'forecast',  
